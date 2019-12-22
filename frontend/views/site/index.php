9dcbc57ac4763f43315f15ccd5f30cb9d9fe81c7
flashkan <nikol-babkin@yandex.ru>
2019-12-23 00:54:25 +0300

    add files for lesson 1

---
+++ frontend/views/site/index.php
@@ -0,0 +1,14 @@
+<?php
+
+/* @var $this yii\web\View */
+
+$this->title = 'Task tracker';
+?>
+<div class="site-index">
+    <div class="jumbotron">
+        <h1>My Task tracker</h1>
+        <p class="lead">Hear will be my task tracker</p>
+        <?= \yii\helpers\Html::img('http://ww2.cs.fsu.edu/~smitha/Images/TaskTracker.png', ['alt' => 'My logo', 'width' => '100%']) ?>
+    </div>
+</div>
+
